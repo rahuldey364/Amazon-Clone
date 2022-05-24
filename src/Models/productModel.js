007b61e16aea8df83088c2a442217d3bbb519d17
Pritam0044 <84521443+Pritam0044@users.noreply.github.com>
2022-05-25 01:28:04 +0530

    product model added

---
+++ src/Models/productModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+
+
+const urlSchema = new mongoose.Schema(
+
+    { 
+        title: {type:String, require:true, unique:true,trim:true},
+        description: {type:String, require:true, trim:true},
+        price: {type:Number, require:true,trim:true},         //////------ valid number/decimal
+        currencyId: {type:String, require:true},            //////---------, INR
+        currencyFormat: {typr:String, require:true},                //////-----------, Rupee symbol
+        isFreeShipping: {type:Boolean, default: false},
+        productImage: {type:String, require:true,trim:true},  // s3 link
+        style: {type:String,trim:true},
+        availableSizes: {type:[String],  enum:["S", "XS","M","X", "L","XXL", "XL"]},           /////------at least one size,
+        installments: {type:Number},
+        deletedAt: {Date},              //////--------, when the document is deleted 
+        isDeleted: {type:Boolean, default: false}
+      },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model('Products',urlSchema);
